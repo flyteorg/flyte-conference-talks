d0516ed72c481c47de751f7e328ea68c77d31aa1
Niels Bantilan <niels.bantilan@gmail.com>
2022-12-03 09:56:41 -0500

    updates
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- pydata-global-2022/workflows/utils.py
+++ pydata-global-2022/workflows/utils.py
@@ -3,8 +3,11 @@ from flytekit.configuration import Config
 from flytekit.remote import FlyteRemote, FlyteWorkflowExecution
 
 
-def get_remote():
-    is_sandbox = os.environ.get("UNION_SANDBOX_PLACEHOLDER_SERVICE_HOST") is not None
+def get_remote(local=None):
+    if local is None:
+        is_sandbox = os.environ.get("UNION_SANDBOX_PLACEHOLDER_SERVICE_HOST") is not None
+    else:
+        is_sandbox = not local
     return FlyteRemote(
         config=Config.auto(config_file=None if is_sandbox else "./config.yaml"),
         default_project="flytesnacks",
