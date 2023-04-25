1aea59bbbe277c3b437170acc0ec11916be4edb2
eduardo apolinario <eapolinario@users.noreply.github.com>
2023-04-25 16:06:10 -0700

    Use docker buildx to build and push images
    Signed-off-by: eduardo apolinario <eapolinario@users.noreply.github.com>

--- pydata-seattle-2023/setup.py
+++ pydata-seattle-2023/setup.py
@@ -4,10 +4,10 @@ with open("requirements.txt") as f:
     install_requires = [x.strip() for x in f.readlines()]
 
 setup(
-    name='pydata-global-2022-tutorial',
+    name='pydata-seattle-2023-tutorial',
     version='0.0.0',
     url='https://github.com/flyteorg/flyte-conference-talks',
-    author='Niels Bantilan',
+    author='Eduardo Apolinario',
     packages=find_packages(),    
     install_requires=install_requires,
 )
