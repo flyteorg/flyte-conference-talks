27b9a02d71ff0ef31d53ad0647b9c5fc4e501fac
eduardo apolinario <eapolinario@users.noreply.github.com>
2023-04-25 18:51:34 -0700

    Simplify local setup
    Signed-off-by: eduardo apolinario <eapolinario@users.noreply.github.com>

--- pydata-seattle-2023/local-setup.sh
+++ pydata-seattle-2023/local-setup.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-python -m venv .venv
-. .venv/bin/activate
-
 VERSION=1.5.0
-pip install jupyter modin pyspark torch pandas scikit-learn \
-    flytekit==$VERSION flytekitplugins-deck-standard==$VERSION flytekitplugins-pandera==$VERSION flytekitplugins-spark==$VERSION flytekitplugins-ray==$VERSION
-pip install -e .
+pip install -r requirements.txt
+pip install jupyter
