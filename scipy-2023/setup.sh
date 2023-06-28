4ffbaaf9b51475ce1defc240a82a1f02bd52d2f8
Niels Bantilan <niels.bantilan@gmail.com>
2023-06-28 17:54:33 -0400

    update scipy 2023 deps
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- scipy-2023/setup.sh
+++ scipy-2023/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 pip install --no-cache-dir --user modin
-pip install --no-cache-dir --user pyspark
-pip install --no-cache-dir --user torch
+pip install --no-cache-dir --user pyspark<3.4.0
+pip install --no-cache-dir --user --index-url https://download.pytorch.org/whl/cpu torch
 pip install --no-cache-dir --user pandas
 pip install --no-cache-dir --user scikit-learn
 pip install --no-cache-dir --user 'flytekit>=1.7.0'
@@ -10,5 +10,4 @@ pip install --no-cache-dir --user flytekitplugins-deck-standard
 pip install --no-cache-dir --user flytekitplugins-pandera
 pip install --no-cache-dir --user flytekitplugins-spark
 pip install --no-cache-dir --user flytekitplugins-ray
-pip install --no-cache-dir --user -r requirements.txt
 pip install --no-cache-dir --user dataclasses_json joblib palmerpenguins wheel
